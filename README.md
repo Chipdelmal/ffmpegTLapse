f52474e64f5020a95c82555aac286eb0bab12c38
chipdelmal <chipdelmal@gmail.com>
2023-09-22 09:02:42 -0700

    More work on full script with args

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 * https://marketsplash.com/tutorials/bash-shell/bash-shell-arguments/
 * https://ffmpeg.org/ffmpeg.html#Main-options
 * https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash
+* https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
--- dev.sh
+++ dev.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-
 ###############################################################################
 # Constants
 ###############################################################################
 CRED='\033[0;31m'
+CGRN='\033[0;32m'
 CPRP='\033[0;35m'
 COFF='\033[0m'
 # FFMPEG ----------------------------------------------------------------------
 TUNE="film"
 PRESET="slow"
+CRF=20
 ###############################################################################
-# Mandatory filename argument
+# Parse arguments
 ###############################################################################
-FNAME=$1
-###############################################################################
-# Optional arguments
-###############################################################################
-OVERWRITE='-n'
+OVERWRITE='n'
 VERBOSE='error'
 FRATE='30'
 BPATH=~/Pictures/GoPro
 ANGLE="1440x1080"
 EXTENSION="JPG"
+FNAME=""
+DEBUG="False"
 # Help message ----------------------------------------------------------------
 print_usage() {
-  printf "
+  printf "$CRED
   * Mandatory argument:
     \t -f: Folder/File name 
   * Optional arguments:
@@ -34,33 +33,72 @@ print_usage() {
     \t -v: Verbose ('panic', 'warning', default: 'error')
   * Optional flags:
     \t -y: Overwrite present video files (off by default).
-    \t -w: Wide-angle fimelapse (linear by default).
+    \t -w: Wide-angle fimelapse (linear by default).$COFF\n
   "
 }
 # Parse optional arguments ----------------------------------------------------
-while getopts 'hywv:r:p:' flag; do
+while getopts 'dhywv:r:p:f:' flag; do
   case "${flag}" in
-    y) OVERWRITE='-y' ;;
+    y) OVERWRITE='y' ;;
     p) BPATH="${OPTARG}" ;;
+    f) FNAME="${OPTARG}" ;;
     v) VERBOSE="${OPTARG}" ;;
     r) FRATE="${OPTARG}" ;;
     w) ANGLE="1920x1080" ;;
+    d) DEBUG="True" ;;
     *) print_usage
        exit 1 ;;
   esac
 done
 # Print for debugging ---------------------------------------------------------
-printf "
-    Overwrite: $OVERWRITE
-    Verbose: $VERBOSE
-    Framerate: $FRATE
-    Filename: $FNAME
-    Angle: $ANGLE
-    Path: $BPATH
-"
+if [ "$DEBUG" = "True" ]; then
+    printf "$CPRP[-d] Debug: $DEBUG
+[-y] Overwrite: $OVERWRITE
+[-v] Verbose: $VERBOSE
+[-r] Framerate:  $FRATE
+[-w] Angle: $ANGLE
+[-p] Path: $BPATH 
+[-f] Filename: $FNAME $COFF\n"
+    exit 0
+fi
+# Exit if filename was not provided -------------------------------------------
+if [ "$FNAME" = "" ]; then
+    printf "$CRED Filename not provided, please provide the foldername with the -f flag! $COFF"
+    exit 0
+fi
+# Working directories ---------------------------------------------------------
+CDIR="$PWD"
+MDIR="${BPATH}/${FNAME}"
 ###############################################################################
 # Move files to same parent folder
 ###############################################################################
-echo -e "${CRED}* Moving files to parent directory from: ${BPATH}/${FNAME} ${COFF}"
+echo -e "${CGRN}[-] Moving files to parent directory from: ${BPATH}/${FNAME} ${COFF}"
 find $BPATH/$FNAME -mindepth 2 -type f \
     -exec mv {} $BPATH/$FNAME \;
+###############################################################################
+# Generate video
+###############################################################################
+echo -e "${CGRN}[-] Compiling video to: ${BPATH}/${FNAME}.mp4 ${COFF}"
+cd $MDIR
+ffmpeg -framerate $FRATE \
+    -pattern_type glob -i "*.$EXTENSION" \
+    -vcodec h264 $BPATH/$FNAME".mp4" \
+    -r $FRATE \
+    -tune $TUNE \
+    -preset $PRESET \
+    -hide_banner  \
+    -loglevel $VERBOSE \
+    -c:v libx264 \
+    -crf $CRF \
+    -async 1 -vsync 1 \
+    -$OVERWRITE
+cd $CDIR
+###############################################################################
+# Postprocess video
+###############################################################################
+echo -e "${CGRN}[-] Postprocessing to: ${BPATH}/${FNAME}-PP.mp4 ${COFF}"
+ffmpeg -i $BPATH/$FNAME".mp4" \
+    -vf "pp=al, scale=$ANGLE" $BPATH/$FNAME"-PP.mp4" \
+    -loglevel $VERBOSE \
+    -hide_banner  \
+    -$OVERWRITE
